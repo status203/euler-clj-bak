2f2a79ce9ea758876c6af4dce81d0c9dc5fa1543
status203 <git@status203.com>
2012-06-27 13:48:04 +0100

    lein2ified

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject euler "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.4.0"]
-                     [lein-midje "1.0.10"]])
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}})
+;;  :dev-dependencies [[midje "1.4.0"]])
