9551005c5c52b7dc725fb830640a69a6f11c7d93
Adam Rulli-Gibbs <git@status203.com>
2015-02-04 16:32:20 +0000

    iterative sieve completed but slower than naive :(

--- src/euler_clj/math/primes.clj
+++ src/euler_clj/math/primes.clj
@@ -1,4 +1,5 @@
-(ns euler-clj.math.primes)
+(ns euler-clj.math.primes
+  (:require [clojure.data.priority-map :refer [priority-map]]))
 
 (defn- divides? ; requiring math.numbers creates a circular dependency
   "Whether integer n divides integer m without remainder"
@@ -35,6 +36,52 @@
                             (lazy-seq (cons next-prime (r-primes next-prime next-primes)))))
                         5 [2 3 5])))
 
+;; sieve passed around as
+;; [vec-of-primes-so-far lazy-seq-of-candidates priority-map-of-multiples]
+
+(defn sieve-update-lowest-multiple
+  "Adds the relevant amount to the first counter"
+  [[_ _  multiples :as sieve]]
+  (let [[prime multiple] (peek multiples)]
+    (assoc-in sieve [2 prime] (+ multiple prime))))
+
+(defn sieve-counters-for-next-candidate
+  "Repeatedly update lowest multiple until lowest multiple is >= candidate
+  (does not update primes or candidates)"
+  [[_ [candidate :as candidates]  _ :as sieve]]
+  (let [[_ _ new-multiples :as new-sieve] (sieve-update-lowest-multiple sieve)]
+    (if (< (second (peek new-multiples)) candidate)
+      (recur new-sieve)
+      new-sieve)))
+
+(defn sieve-next-candidate-update
+  "Updates all components of the sieve for the next candidate"
+  [[_ [candidate & rest-candidates]  multiples :as sieve]]
+    (-> sieve
+        sieve-counters-for-next-candidate
+        (assoc-in [1] rest-candidates)
+        ((fn [[primes _ multiples :as sieve]]
+           (if (< candidate (second (peek multiples)))
+             (-> sieve
+                 (update-in [0] #(conj % candidate))
+                 (update-in [2] #(assoc % candidate candidate)))
+             sieve)))))
+
+(defn prime-sieves
+  "Returns a lazy sequence of sieves, each of which 'finds' a new prime"
+  ([] (let [initial-sieve [[2] (iterate #(+ 2 %) 3) (priority-map 2 2)]]
+        (prime-sieves 2 initial-sieve)))
+  ([prev-prime prev-sieve]
+   (let [next-sieve (sieve-next-candidate-update prev-sieve)
+         next-sieve-last-prime (peek (first next-sieve))]
+     (if (= next-sieve-last-prime prev-prime)
+       (prime-sieves prev-prime next-sieve)
+       (lazy-seq (cons next-sieve (prime-sieves next-sieve-last-prime next-sieve)))))))
+
+(defn primes-by-iterative-sieve
+  "Lazy sequence of primes using an iterative sieve"
+  [] (cons 2 (map #(last (first %)) (prime-sieves))))
+
 (def primes primes-by-prior-primes)
 
 (defn prime-factors
@@ -50,6 +97,4 @@
                     (conj factors possible-factor)
                     possible-factors)
              (recur m factors (rest possible-factors)))))))
-
-
 
--- test/euler_clj/math/primes_test.clj
+++ test/euler_clj/math/primes_test.clj
@@ -1,6 +1,7 @@
 (ns euler-clj.math.primes-test
   (:require [euler-clj.math.primes :refer :all]
-            [midje.sweet :refer [tabular fact facts just]]))
+            [midje.sweet :refer [tabular fact facts just]]
+            [clojure.data.priority-map :refer [priority-map]]))
 
 (tabular
  (facts "about primeness"
@@ -21,18 +22,24 @@
                                31 37 41 43 47
                                53 59 61 67 71])
 
-#_(tabular
+(tabular
   (facts "about finding the next prime"
          (prime-next ?n ?primes) => ?result)
 
  ?primes ?n ?result
  []      1  2)
 
-(fact "about first twenty primes"
- (take 20 (primes)) => [ 2  3  5  7 11
-                        13 17 19 23 29
-                        31 37 41 43 47
-                        53 59 61 67 71])
+(fact "about first twenty primes naively calculated"
+ (take 20 (primes-naive)) => [ 2  3  5  7 11
+                              13 17 19 23 29
+                              31 37 41 43 47
+                              53 59 61 67 71])
+
+(fact "about first twenty primes calculated using previous primes"
+ (take 20 (primes-by-prior-primes)) => [ 2  3  5  7 11
+                                        13 17 19 23 29
+                                        31 37 41 43 47
+                                        53 59 61 67 71])
 
 (tabular
  (facts "about prime factors"
@@ -44,4 +51,34 @@
   5    [5]
   6    [2 3]
  12    [2 2 3])
+
+(tabular
+ (facts "about updating the lowest multiple in an iterative sieve"
+        (sieve-update-lowest-multiple ?sieve) => ?result)
+
+ ?sieve                                   ?result
+ [[2] [3 5 7] (priority-map 2 2)]         [[2] [3 5 7] {2 4}]
+ [[2 3] [5 7 9] (priority-map 2 4 3 3)]   [[2 3] [5 7 9] {2 4, 3 6}])
+
+(tabular
+ (facts "about updating sieve multiples for the next candidate"
+        (sieve-counters-for-next-candidate ?sieve) => ?result)
+
+ ?sieve                                   ?result
+ [[2 3] [5 7 9] (priority-map 2 4 3 3)]  [[2 3] [5 7 9] {2 6, 3 6}])
+
+#_(tabular
+ (facts "about updating all sieve components for the next candidate"
+        (sieve-next-candidate-update ?sieve) => ?result)
+
+ ?sieve                                                         ?result
+ [[2 3] (seq [5 7 9]) (priority-map 2 4 3 3)]                   [[2 3 5] (7 9) {5 5, 2 6, 3 6}]
+ [[2 3 5] (seq [7 9 11 13]) (priority-map 2 6 3 6 5 5)]         [[2 3 5 7] (9 11 13) {7 7, 2 8, 3 9, 5 10}]
+ [[2 3 5 7] (seq [9 11 13 15]) (priority-map 2 8 3 9 5 10 7 7)] [[2 3 5 7] (11 13 15) {3 9, 2 10, 5 10, 7 14}])
+
+(fact "about first twenty primes calculated using iterative sieve"
+ (take 20 (primes-by-iterative-sieve)) => [ 2  3  5  7 11
+                                            13 17 19 23 29
+                                            31 37 41 43 47
+                                            53 59 61 67 71])
 
