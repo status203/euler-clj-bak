e8bf8622378fb4aa95de16e87c98760293dc3b25
Adam Rulli-Gibbs <git@status203.com>
2015-02-19 14:00:18 +0000

    Problem 11 done. Creaded sofs library. OTT?

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/data.priority-map "0.0.5"]]
+                 [org.clojure/data.priority-map "0.0.5"]
+                 [sofs "0.1.0-SNAPSHOT"]]
   :profiles     {:dev {:dependencies [[midje "1.6.3"]
                                       [criterium "0.4.3"]]}})
 
